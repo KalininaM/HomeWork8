54a14a0f9d07a7f06748495818777dad8fb11d77
Marina <shalneva_mg@mail.ru>
2023-04-19 21:54:07 +0400

    Added program 54 task

---
+++ Task54/Program.cs
@@ -0,0 +1,65 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+
+int[,] array = CreateArray();
+Console.WriteLine("Изначальный массив:");
+PrintArray(array);
+Console.WriteLine("Упорядоченный массив по убыванию каджого элемента в строке:");
+int[,] desArray = OrderingArrayInDescending(array);
+PrintArray(desArray);
+
+int[,] OrderingArrayInDescending(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+
+    return array;
+}
+
+int [,] CreateArray ()
+{
+    int [,] Array = new int [3, 4];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i,j] = new Random().Next(1, 16);
+        }
+    }
+    return Array;
+}
+
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write(Array[i,j]+" ");
+        }
+    Console.WriteLine();
+    }
+}
+
